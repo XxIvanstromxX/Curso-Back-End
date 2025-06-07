8a2eeb353d4674ce650d4d417f81ceef1b436d75
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-06-06 22:21:45 -0600

    Agregar archivo condicionales.js con ejemplos de condicionales if y if-else

---
+++ Clase-2/condicionales.js
@@ -0,0 +1,38 @@
+//Condicional if
+
+/*let edad = 18;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+}
+console.log("Hola, soy un mensaje que se ejecuta siempre");*/
+
+//Condicional if-else
+
+// let numero1 = 10;
+// let numero2 = 0;
+
+// let div = numero1 / numero2;
+// console.log(`El resultado de la división es: ${div}`);
+
+// if (numero1 !== 0 && numero2 !== 0) {
+//   let division = numero1 / numero2;
+//   console.log(`El resultado de la división es: ${division}`);
+// } else {
+//   console.log("No se puede dividir por cero");
+// }
+
+//Condicional if-else
+// let edad = 15;
+
+// if (edad >= 18) {
+//   console.log("Eres mayor de edad");
+// } else if (edad >= 13 && edad < 18) {
+//   console.log("Eres un adolescente");
+// } else if (edad >= 60) {
+//   console.log("Eres un adulto mayor");
+// } else {
+//   console.log("Eres un niño");
+// }
+
+// console.log("Hola, soy un mensaje que se ejecuta siempre");
