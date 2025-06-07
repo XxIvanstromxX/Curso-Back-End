a64412f44d621af85bbe92eb75c0df5b5d18c6ff
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-06-06 22:21:56 -0600

    Agregar bucles.js con ejemplos de bucles for y while

---
+++ Clase-2/bucles.js
@@ -0,0 +1,33 @@
+// console.log("numero 0");
+// console.log("numero 1");
+// console.log("numero 2");
+// console.log("numero 3");
+// console.log("numero 4");
+// console.log("numero 5");
+// console.log("numero 6");
+// console.log("numero 7");
+// console.log("numero 8");
+// console.log("numero 9");
+// console.log("numero 10");
+// console.log("numero 11");
+// console.log("numero 12");
+// console.log("numero 13");
+// console.log("numero 14");
+// console.log("numero 15");
+// console.log("numero 16");
+
+// Bucle for
+
+/*for (let i = 0; i < 17; i++) {
+  console.log(`Numero ${i}`);
+}
+
+console.log("termino el bucle for");*/
+
+// Bucle while
+let i = 0;
+while (i < 17) {
+  console.log(`Numero ${i}`);
+  i++;
+}
+console.log(`El ultimo numero es ${i}`);
