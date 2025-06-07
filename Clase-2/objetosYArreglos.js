4e9bf6c5affcee45235e0ea8e36ba1ef435884c4
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-06-06 22:22:12 -0600

    Agregar archivo objetosYArreglos.js con ejemplos de arreglos y objetos en JavaScript

---
+++ Clase-2/objetosYArreglos.js
@@ -0,0 +1,28 @@
+//Arreglo ES UNA LISTA DE VALORES QUE TIENE UN TIPO DE DATO Y UN INDICE
+
+// let arreglo2 = ["Hola", "Mundo", "JavaScript"]; // Arreglo de cadenas de texto
+// let arreglo3 = [true, false, true]; // Arreglo de booleanos
+// let arreglo4 = [1, "Hola", true, null]; // Arreglo mixto (no recomendado)
+// let arreglo5 = [
+//   [1, 2],
+//   [3, 4],
+//   [5, 6],
+// ]; // Arreglo de arreglos (matriz)
+// let arreglo = [24, 12, 34, 56, 78, 90]; // Arreglo de números
+
+// // Acceso a elementos del arreglo
+// console.log(arreglo[3]);
+// //Acceso a la matriz
+// console.log(arreglo5[2][1]); // Accede al primer elemento del segundo arreglo dentro de la matriz
+// //Acceder a un elemento que no existe
+// console.log(arreglo[10]); // Esto devolverá undefined porque el índice 10 no existe en el arreglo
+
+//Objetos es una colección de pares clave-valor
+let objeto = {
+  nombre: "Juan",
+  edad: 30,
+  ciudad: "Madrid",
+  lenguajes: ["JavaScript", "Python", "Java"],
+};
+// Acceso a propiedades del objeto
+console.log(objeto.lenguajes[1]);
